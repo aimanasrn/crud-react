d4d8abb1e1a71f118cd14f4fc9a42cd25c96b5ab
Muhammad Aiman bin Kanasronsham <96905225+aimanasrn@users.noreply.github.com>
2025-05-29 16:32:32 +0800

    front end

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  console.log('App component rendered')
+  return (
+    <>
+      <div className='bg-amber-200'>Hellow world</div>
+    </>
+  )
+}
+
+export default App
+ 
